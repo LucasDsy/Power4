016b90e7a73b3417be3850a47107a271188cc0fe
sun <lucas.dansay@gmail.com>
2020-06-08 16:56:08 +0200

    Création première vue

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,11 +1,14 @@
 package app;
 
-public class App extends Application
+import view.Interface;
+
+public class App
 {
 
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        Interface my_interface = new Interface();
+        my_interface.initInterface();
     }
+
     
 }
