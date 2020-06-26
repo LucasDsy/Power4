b401fb5b557f532a9bb7b7a5104210105a0007e4
lucas.dansay@gmail.com <lucas.dansay@gmail.com>
2020-06-26 12:24:57 +0200

    ajustement gameModel

--- src/main/java/app/model/GameModel.java
+++ src/main/java/app/model/GameModel.java
@@ -5,8 +5,19 @@ import app.model.enums.State;
 public class GameModel {
     private State currentPlayer;
 
+    private int cols = 7;
+    private int rows = 6;
+
     public GameModel() {
         this.currentPlayer = State.P1;
+    }
+
+    public int getNbCols() {
+        return this.cols;
+    }
+
+    public int getNbRows() {
+        return this.rows;
     }
 
     public void nextPlayer() {
